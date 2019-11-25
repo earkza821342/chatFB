ce89813c18d72d92109fb14082bf69b422da875b
Worawut Niamsiri <niamsiri.em@gmail.com>
2019-11-25 14:57:35 +0700

    update all

--- construct.js
+++ construct.js
@@ -2,7 +2,7 @@ module.exports = {
     LOGS_PATH: "./src/assets/logs",
     PICTURE_DIR: "./src/assets/images",
     FILES_DIR: "./src/assets/files",
-    MONGO_DB: "mongodb://localhost:27017/mydb",
+    MONGO_DB: "mongodb://chat:chatDev@18.139.0.200:27017/chat_db",
     BASE_URL: "http://localhost:8080",
     REDIS_URL: "",
     CERTIFICATE_KEY: {},
