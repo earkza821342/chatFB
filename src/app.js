85b1969a74f9446423c41b88dda3e9e433d59621
Worauwt <niamsiri.em@gmail.com>
2019-12-01 22:40:05 +0700

    fixed files

--- src/app.js
+++ src/app.js
@@ -3,70 +3,13 @@ const http = require("http")
 const options = require("../construct").CERTIFICATE_KEY
 const app = require("./config/express")
 const init = require("./sockets/init")
-const bodyParser = require('body-parser')
-const express = require('express')
-appP = express().use(bodyParser.json()); 
 
 init.startSocketServer()
 
-http.createServer(app).listen(8080, function () {
-    console.log("Server Start HTTP !!!!! ")
+http.createServer(app).listen(80, function () {
+  console.log("Server Start HTTP !!!!! ")
 })
 
-https.createServer(options, app).listen(8081, function () {
-    console.log("Server Start HTTPS !!!!! ")
+https.createServer(options, app).listen(443, function () {
+  console.log("Server Start HTTPS !!!!! ")
 })
-
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-app.post('/webhook', (req, res) => {  
- 
-    let body = req.body;
-  
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-  
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-  });
-
-  app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "CHAT-Face-Eark"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-  });
---
+++ src/routes/api/webhook.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express();
+
+router.post('/webhook', (req, res) => {
+    let body = req.body;
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.get('/webhook', (req, res) => {
+    let VERIFY_TOKEN = "CHAT-Face-Eark"
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+module.exports = router
