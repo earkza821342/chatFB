2fc430e2fd1f9d15ceaf2105e0db73e10650b4d7
Worawut Niamsiri <niamsiri.em@gmail.com>
2019-12-03 17:03:35 +0700

    update all

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const router = express();
+const requests = require('request-promise')
 
-router.post('/', (req, res) => {
-    let body = req.body;
-    if (body.object === 'page') {
-        body.entry.forEach(function (entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        res.sendStatus(404);
-    }
+const token = "EAAjo6vKJ6RMBAN9apbt9jWGPjiL3BDvGZAlh1u92UQktAnEYjEUxQvjZCd3oSo0mOCZA2CW5EJJIA0xjY70uW1UJVcCVcXUCfiFtY2T9oYOZCbP1sRa0I0eNOv5O9xqvw3JpERf8QXOrvsKyMHEq3ubuToDpYDuBZAn1bImaIcAZDZD"
+
+router.post('/', (request, response) => {
+    let pageChat = request.body.entry;
+
+    pageChat.forEach((entry) => {
+        if (entry.messaging) {
+            entry.messaging.forEach(msg => {
+                console.log(msg)
+                if (msg.sender.id && msg.message.text) {
+                    sendMessageTest(msg.sender.id)
+                    response.sendStatus(200)
+                }
+            })
+        }
+    });
+    response.status(200).send('EVENT_RECEIVED');
+
 });
 
 router.get('/', (req, res) => {
-    console.log(req);
-    let VERIFY_TOKEN = "CHAT-Face-Eark"
+    let VERIFY_TOKEN = "token-session-facebook"
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
@@ -30,6 +37,19 @@ router.get('/', (req, res) => {
     } else {
         res.sendStatus(403);
     }
-});
+})
+
+sendMessageTest = async (id) => {
+    return requests({
+        url: "https://graph.facebook.com/v5.0/me/messages?access_token=" + token,
+        method: "POST",
+        json: {
+            recipient: {
+                id: id
+            },
+            message: { text: "hello" }
+        }
+    })
+}
 
 module.exports = router
